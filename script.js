ca0421b8700e7e8d3ae7da2eed56a99a9097ef6a
LittleMagician <bthaoxaochay@gmail.com>
2021-01-11 20:29:54 -0600

    added HTML variables to JS

---
+++ script.js
@@ -0,0 +1,7 @@
+var questions = document.querySelector("#questions");
+var answerOne = document.querySelector("#answerOne");
+var answerTwp = document.querySelector("#answerTwp");
+var answerThree= document.querySelector("#answerThree");
+var answerFour = document.querySelector("#answerFour");
+var correctOrwrong = document.querySelector("#correctOrwrong");
+
