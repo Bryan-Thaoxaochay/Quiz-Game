5f0a4b0ba66ee6b45cb168acb28727c4a4400772
LittleMagician <bthaoxaochay@gmail.com>
2021-08-26 15:27:35 -0500

    Commit before beginning to display questions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 import { questions } from './questions.js';
 
-const timer = document.querySelector("#time");
-let time = 60;
+
 document.querySelector("#start-quiz-button").addEventListener("click", startGame);
 
-function startGame() {
-    setInterval(setTimer, 1000);
-}
+const timer = document.querySelector("#time");
+let time = 60;
 
 function setTimer() {
     timer.innerHTML = `Time: ${time}`
     time -= 1;
 }
+
+function startGame() {
+    setInterval(setTimer, 1000);
+
+    // Display the question and answers
+}
+
+console.log(questions);
