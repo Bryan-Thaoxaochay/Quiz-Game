cc86528bc3754ccd0b359a9bd2085836cd40dcd8
LittleMagician <bthaoxaochay@gmail.com>
2021-08-28 18:07:49 -0500

    Finishing quiz leads to score page

--- js/script.js
+++ js/script.js
@@ -3,34 +3,7 @@ import { questions } from './questions.js';
 const introHeader = document.querySelector("#intro");
 const rulesText = document.querySelector("#rules");
 const startButton = document.querySelector("#start-quiz-button");
-
-const questionText = document.querySelector("#question");
-const answersText = document.querySelector("#answers");
-const optionOne = document.querySelector("#option-one");
-const optionTwo = document.querySelector("#option-two");
-const optionThree = document.querySelector("#option-three");
-const optionFour = document.querySelector("#option-four");
-const horizontalLine = document.querySelector("hr");
-const answerIndicator = document.querySelector("#response");
-
-
 startButton.addEventListener("click", startGame);
-
-const timerEl = document.querySelector("#time");
-let time = 60;
-let timer;
-
-
-let questionNumber = 0;
-
-function setTimer() {
-    timerEl.innerHTML = `Time: ${time}`
-    time -= 1;
-    if (time === 0) {
-        alert("Game Over!");
-        return;
-    }
-}
 
 function startGame() {
     timer = setInterval(setTimer, 1000);
@@ -41,12 +14,33 @@ function startGame() {
         if (event.target.tagName === "LI" || event.target.tagName === "BUTTON") {
             questionNumber += 1;
             let userAnswer = event.target.textContent;
-            displayNextQuestion(questionNumber, userAnswer);
+            questionNumber < questions.length ? displayNextQuestion(questionNumber, userAnswer) : quizDone();
         }
     });
 
 }
 
+const timerEl = document.querySelector("#time");
+let time = 60;
+let timer;
+function setTimer() {
+    timerEl.innerHTML = `Time: ${time}`
+    time -= 1;
+    if (time === 0) {
+        alert("Game Over!");
+        return;
+    }
+}
+
+const questionText = document.querySelector("#question");
+const answersText = document.querySelector("#answers");
+const optionOne = document.querySelector("#option-one");
+const optionTwo = document.querySelector("#option-two");
+const optionThree = document.querySelector("#option-three");
+const optionFour = document.querySelector("#option-four");
+const horizontalLine = document.querySelector("hr");
+const answerIndicator = document.querySelector("#response");
+let questionNumber = 0;
 function displayQuestion(questionNumber) {
     // Display the question and answers
     introHeader.style.display = "none";
@@ -80,5 +74,28 @@ function displayNextQuestion(questionNumber, userAnswer) {
         time -= 5;
         timer = setInterval(setTimer, 1000);
     }
+}
 
+const finalHeader = document.querySelector("#ending");
+const scoreText = document.querySelector("#score-text");
+const initialInput = document.querySelector("#initials");
+const saveButton = document.querySelector("#save-score-button");
+function quizDone() {
+    questionText.style.display = "none";
+    answersText.style.display = "none";
+    horizontalLine.style.display = "none";
+    answerIndicator.style.display = "none";
+
+    finalHeader.innerHTML = "You finished the quiz!";
+    scoreText.innerHTML = "Your score is ...";
+    initialInput.innerHTML = "Please input your initials";
+    saveButton.innerHTML = "Save Score";
+
+    finalHeader.style.display = "block";
+    scoreText.style.display = "block";
+    initialInput.style.display = "block";
+    saveButton.style.display = "block";
+
+    timerEl.style.display = "none";
+    clearInterval(timer);
 }
